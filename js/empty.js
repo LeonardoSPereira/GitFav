df8821ee791e7aa0cd49c334788a45b559803112
Leonardo Pereira <leonardo_human@hotmail.com>
2023-08-07 10:41:17 -0300

    empty.js update

--- js/empty.js
+++ js/empty.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export function empty() {
     const tbody = document.querySelector('tbody');
     if(tbody.innerHTML.trim() === '') {
@@ -14,21 +13,4 @@ export function empty() {
         </tr>
         `;
     }
-=======
-export function empty() {
-    const tbody = document.querySelector('tbody');
-    if(tbody.innerHTML.trim() === '') {
-        tbody.innerHTML = 
-        `
-        <tr>
-          <td colspan="4" class="empty">
-            <div >
-              <img src="./assets/Estrela.svg" alt="Imagem de uma estrela">
-              <p>Nenhum favorito ainda</p>
-            </div>
-          </td>
-        </tr>
-        `;
-    }
->>>>>>> e231833fb1d35be174b05e011c3ca1fdd5d7170e
 }
